352456799d64dae00a1c3af769deaa4ac615f1f9
jonhenryf <jonhenryf@gmail.com>
2019-03-13 01:00:38 +0000

    Fix readline, implementing on parse.

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,51 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <ostream>
-#include <string>
-#include <stdlib.h>
+#include <string.h>
+#include <cstdlib>
 #include <cstring>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define token_buffsize 64 // for parse function
+#define delimiter " \t\r\n\a"
 
-
-char readline(void) {
-
+char *readline()
+{
+    char *line = NULL;
+    ssize_t buffersize = 0;
+    cin.getline(line, buffersize);
+    return line;
 }
 
-char parseline () {
+char parseline (char *line) {
+    int buffersize = token_buffsize;
+    int position = 0;
+    char **tokens = (char**)malloc(buffersize * sizeof(char*));
+    char *token;
+    char **tokensback;
 
-    int token_buffer_size = 64;
+    token = strtok(line, delimiter);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= buffersize) {
+            buffersize += token_buffsize;
+            tokensback = tokens;
+            tokens = (char**)realloc(tokens, buffersize * sizeof(char*));
+        }
+
+        token = strtok(NULL, delimiter);
+
+    }
+
+    token[position] = NULL;
+    return tokens;
 
 }
 
@@ -33,7 +61,7 @@ void unixloop(void) {
     int stat;
 
     do {
-        std::cout << ("BB> ") << std::endl;
+        cout << ("BB> ") << std::endl;
         line = readline();
         arguments = parseline(line);
         stat = execute(arguments);
