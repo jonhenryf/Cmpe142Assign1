ada39854df1a2005bd008421be4569ac350e8489
jonhenryf <jonhenryf@gmail.com>
2019-03-22 23:41:59 +0000

    Basic Shell implemented and working. CD implemented.

--- Main.cpp
+++ Main.cpp
@@ -1,56 +1,127 @@
-//
-// Created by jonhenryf on 3/12/19.
-//
-
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <iostream>
+#include <err.h>
+#include <fstream>
 #include <ostream>
-#include <string>
-#include <stdlib.h>
+#include <string.h>
+#include <cstdlib>
 #include <cstring>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+#define token_buffersize 64
+#define token_delim " \t\r\n\a"
 
-char readline(void) {
+char ** parseline( char *line) {
+    int buffersize = token_buffersize;
+    int index = 0;
+    char **command = (char**)malloc(buffersize * sizeof(char*));
+    char *token;
 
-}
-
-char parseline () {
-
-    int token_buffer_size = 64;
-
-}
-
-int execute() {
-
-}
-
-void unixloop(void) {
-    char * line;
-    char ** arguments;
-    int stat;
-
-    do {
-        std::cout << ("BB> ") << std::endl;
-        line = readline();
-        arguments = parseline(line);
-        stat = execute(arguments);
-
-        free(line);
-        free(arguments);
+    if (!command) {
+        cout << "allocation error" << endl;
+        exit(EXIT_FAILURE);
     }
 
-    while (stat);
+    token = strtok(line, token_delim);
+    while (token != NULL) {
+        command[index] = token;
+        index ++;
+
+        if (index >= buffersize) {
+            buffersize += token_buffersize;
+            command = (char**)realloc(command, buffersize * sizeof(char*));
+            if (!command) {
+                cout << stderr << "allocation error" << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, token_delim);
+    }
+
+    command[index] = NULL;
+    return command;
 
 }
 
-int main() {
+void execute (char** command) {
 
-        unixloop();
-        return 0;
+    pid_t pid = fork();
+    int status;
+
+    if (pid == -1) {
+        cout << "Failed to fork" << endl;
+        return;
+    }
+
+    else if (pid == 0) {
+        if (execv(command[0], command) < 0) {
+            cout << " Could not execute" << endl;
+        }
+
+        exit(0);
+    }
+
+    else {
+        waitpid(pid, &status, WUNTRACED);
+        return;
+    }
+
 
 }
+
+
+int main(int argc, char **argv) {
+    char **command;
+    char *line = NULL;
+    size_t linesize = 0;
+
+
+    while (1) {
+        cout << "Jontan@root> ";
+        getline(&line, &linesize, stdin);
+
+        command = parseline(line);
+
+        if (!strcmp(command[0], "cd")) {
+            char s[100];
+            char* currentdir = getcwd(s, 100);
+
+            if(command[1] == NULL) {
+                cout << currentdir << endl;
+            }
+
+            else {
+                chdir(command[1]);
+                currentdir = getcwd(s, 100);
+                cout << currentdir << endl;
+            }
+        }
+
+        else if (!strcmp(command[0], "path")) {
+
+
+        }
+
+        else if (!strcmp(command[0], "exit")) {
+            exit(0);
+        }
+
+        else {
+
+            execute(command);
+
+        }
+    }
+}
+
+
+
+
+
 
 
